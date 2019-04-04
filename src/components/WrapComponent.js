ad616860a037dde79a8dd1b9df98d74c99d63e89
李俊 <lijun@lijundeMacBook-Air.local>
2019-04-04 17:57:44 +0800

    update

--- src/components/WrapComponent.js
+++ src/components/WrapComponent.js
@@ -2,8 +2,11 @@ export default function (mapStateToProps) {
     const state = mapStateToProps()
     debugger
     return function (inner) {
-        inner.prototype.mapStateToProps=mapStateToProps;
+        inner.prototype.getName=function () {
+            console.log(666)
+        };
         debugger
+        inner.defaultProps=state;
         return inner;
     }
 }

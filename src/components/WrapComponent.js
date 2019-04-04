a678dad67a404054e458be51ce051895ac843a94
李俊 <lijun@lijundeMacBook-Air.local>
2019-04-04 18:02:47 +0800

    update

--- src/components/WrapComponent.js
+++ src/components/WrapComponent.js
@@ -10,3 +10,21 @@ export default function (mapStateToProps) {
         return inner;
     }
 }
+
+/*
+* 代理拦截
+* */
+export function HOC(WrappedComponent) {
+    return class Test extends Component {
+        render() {
+            const newProps = {
+                title: 'New Header',
+                footer: false,
+                showFeatureX: false,
+                showFeatureY: true
+            }
+
+            return <WrappedComponent {...this.props} {...newProps} />
+        }
+    }
+}
